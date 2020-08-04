ac9eb611697d6f8f2e8cfcae97cda544be648533
kevinchheang <kevin.chh89@gmail.com>
2020-08-04 13:17:50 -0700

    Created User model

---
+++ models.py
@@ -0,0 +1,50 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key=True)
+
+    password = db.Column(db.Text, nullable=False)
+
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+    first_name = db.Column(db.String(30), nullable=False)
+
+    last_name = db.Column(db.String(30), nullable=False)
+
+
+    @classmethod
+    def register(cls, username, password):
+        """Register user w/hashed password & return user."""
+        hashed = bcrypt.generate_password_hash(password)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
